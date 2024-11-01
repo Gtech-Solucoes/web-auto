faa8c8d8046f695a4e58785a5b7e36b1dc870b4d
Giovani Fernandes <gifernandes105@gmail.com>
2024-10-31 23:29:47 -0300

    fix: wpp button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,12 @@ export default function RootLayout({
         {children}
         <Footer />
         <div className="fixed bottom-6 right-6 float-right w-12 h-12 flex justify-center cursor-pointer items-center bg-green-500 rounded-full ">
-          <Image src={wpIcon} width={28} height={28} alt="WhatsApp Icon" />
+          <a
+            href="https://api.whatsapp.com/send?phone=5517988034098&text=Ol%C3%A1%20gostaria%20de%20vender%20ou%20trocar%20meu%20carro.%0APode%20me%20ajudar%3F"
+            target="_blank"
+          >
+            <Image src={wpIcon} width={28} height={28} alt="WhatsApp Icon" />
+          </a>
         </div>
       </body>
     </html>
